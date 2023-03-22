add530f5d32508be631ea7cd38efb6939a092489
LDHEBONY <cjg0513396@gmail.com>
2023-03-23 03:09:19 +0900

    몬스터스폰 골조
    스폰 및 몬스터 높이에 맞는 스폰위치 구현

---
+++ NinjaExocist2d/Assets/Script/ResponPoint.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResponPoint : MonoBehaviour
+{
+
+
+
+    [SerializeField]
+    private GameObject mon1Prefab;
+    [SerializeField]
+    private GameObject mon2Prefab;
+    [SerializeField]
+    private GameObject mon3Prefab;
+    [SerializeField]
+    private GameObject mon4Prefab;
+
+    public float mon1delay = 10.0f;
+    public float mon2delay = 50.0f;
+    public float mon3delay = 100.0f;
+    public float mon4delay = 50.0f;
+
+    [SerializeField]
+    private bool isresponed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {       
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (isresponed == false)
+        {
+            StartCoroutine(Spawn(mon1delay, mon1Prefab));
+
+            StartCoroutine(Spawn(mon2delay, mon2Prefab));
+
+            StartCoroutine(Spawn(mon3delay, mon3Prefab));
+
+            StartCoroutine(Spawn(mon4delay, mon4Prefab));
+        }
+
+            
+
+    }
+
+    private IEnumerator Spawn(float delay, GameObject monster)
+    {
+        isresponed = true;
+        yield return new WaitForSecondsRealtime(delay);
+        Vector2 spawnvec = new Vector2(this.transform.position.x, this.transform.position.y);
+        if(monster.name == "Monster1")
+        {
+            spawnvec.y += 6f; 
+        }
+        if(monster.name == "Monster2")
+        {
+            spawnvec.y += 3f;
+        }
+
+        GameObject newenemy = Instantiate(monster, spawnvec, Quaternion.identity);
+        isresponed = false;
+
+        //StartCoroutine(Spawn(delay, monster));
+    }
+}
